10c1fd204b89e775221fc4113efc73bda23b3f40
Michael Maurer <mjmaurer777@gmail.com>
2022-11-19 17:43:32 -0500

    fix poetry path

--- keyframes.py
+++ keyframes.py
@@ -1,1 +1,11 @@
+class Timeline:
+    def __init__(self) -> None:
+        pass
 
+
+class Scene:
+    def __init__(self, bpm, fps) -> None:
+        pass
+
+    def new_timeline(self) -> Timeline:
+        pass
