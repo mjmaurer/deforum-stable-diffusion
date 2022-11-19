eac7aaf95d8f068f210aed07406e91e0ce6ff217
Michael Maurer <mjmaurer777@gmail.com>
2022-11-19 01:16:35 -0500

    fix path

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -60,7 +60,7 @@ animation_prompts = {
     30: "a beautiful coconut, trending on Artstation",
     40: "a beautiful durian, trending on Artstation",
 }
-base = "/storage"
+base = "/storage/deforumsd"
 input_base = f"{base}/input"
 batch_name = "StableFun" #@param {type:"string"}
 setup_environment = True #@param {type:"boolean"}
