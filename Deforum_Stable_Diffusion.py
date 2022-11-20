d11e76d911b3bb6385eb146cabe0055b8d76a12a
Michael Maurer <mjmaurer777@gmail.com>
2022-11-20 15:16:11 -0500

    fix zoom and angel

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -113,11 +113,12 @@ def DeforumAnimArgs():
     perspective_flip_gamma = "0:(0)"#@param {type:"string"}
     perspective_flip_fv = "0:(53)"#@param {type:"string"}
     noise_schedule = "0: (0.02)"#@param {type:"string"}
-    zoom = "0:(1.04)"#@param {type:"string"}
-    angle = "0:(1)"#@param {type:"string"}
     # strength_schedule = "0: (1.0), 70: (1.0), 200: (0.65), 400: (0.6), 500: (0.5)"#@param {type:"string"}
     # blend_schedule = "0: (1), 200: (1), 320: (0.6), 500: (0.1)"#@param {type:"string"}
     # I think stren should reset when we change to iter
+    # TODO maybe introduce a bit of the blend. See the second vid in doc
+    zoom = "0:(0), 201:(1.04)" #@param {type:"string"}
+    angle = "0:(0), 201:(1)" #@param {type:"string"}
     strength_schedule = "0: (1), 75: (1), 200: (0.5), 201: (0.99), 220: (0.5)" #@param {type:"string"}
     blend_schedule = "0: (1), 200: (1), 201: (0)"#@param {type:"string"}
     contrast_schedule = "0: (1.0)"#@param {type:"string"}
