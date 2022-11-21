d683fe0515b70c9ebefd54e1c4f0dffe76b11223
Michael Maurer <mjmaurer777@gmail.com>
2022-11-20 21:42:14 -0500

    angle

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -112,7 +112,7 @@ def DeforumAnimArgs():
     # TODO try should and long blend_build
     blend_build = 1 # 100
     zoom = f"0:(1), {switch_frame - 1}:(1), {switch_frame}:(1.005)" #@param {type:"string"}
-    angle = f"0:(0), {switch_frame - 2}:(0), {switch_frame - 1}:(0.5)" #@param {type:"string"}
+    angle = f"0:(0), {switch_frame - 2}:(0), {switch_frame - 1}:(0.4)" #@param {type:"string"}
     strength_schedule = f"0: (1), {switch_frame - strength_build}: (1), {switch_frame}: (0.45), {switch_frame + 200}: (0.6)" # {switch_frame}: (0.7), {switch_frame + 200}: (0.55)" #@param {type:"string"}
     blend_schedule = f"0: (1), {switch_frame - blend_build}: (1), {switch_frame}: (0.95), {switch_frame + 1}: (0) "#@param {type:"string"}
     contrast_schedule = "0: (1.0)"#@param {type:"string"}
