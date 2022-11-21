af1da89e33c4a3ac0156a472bfa49cba2702bf51
Michael Maurer <mjmaurer777@gmail.com>
2022-11-20 20:51:17 -0500

    prompt

--- Deforum_Stable_Diffusion.py
+++ Deforum_Stable_Diffusion.py
@@ -5,7 +5,7 @@ ENV = os.environ
 
 vid_strength = float(ENV.get("STRENGTH", 1))
 video_file_name = ENV.get("VID_FILE", "19testvid.mp4")
-vid_prompt = ENV.get("VID_PROMPT", "a web of brown-orange branches and leaves, oil painting, high detail, trippy, psychedelic")
+vid_prompt = ENV.get("VID_PROMPT", "a web of brown-orange branches and leaves in the forest, oil painting, high detail, trippy, psychedelic")
 enhanced_vid_mode = ENV.get("VID_MODE_OFF", True)
 # Set to overwrite video inputframes if they already exist
 rewrite_video_frames = ENV.get("REWRITE_VIDEO_FRAMES", False)
